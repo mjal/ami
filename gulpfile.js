f7554427c04c05321c9af3a2620af2ccba8dd5f6
Maxime Lalisse <maxime.lalisse@safebook.fr>
2015-11-15 00:54:38 +0100

    gulp inline

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@ var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var addsrc = require('gulp-add-src');
+var inline = require('gulp-inline');
+var minifyCss = require('gulp-minify-css');
 
-gulp.task('coffee', function() {
+gulp.task('js', function() {
   gulp.src(['./src/coffee/config.coffee', './src/coffee/index.coffee'])
     .pipe(concat("application.coffee"))
     .pipe(coffee().on('error', gutil.log))
@@ -16,4 +18,16 @@ gulp.task('coffee', function() {
     .pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('default', ['coffee']);
+gulp.task('css', function() {
+  gulp.src('./src/index.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('dist', function() {
+  gulp.src('./src/index.html')
+    .pipe(inline({disabledTypes: ['img']}))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('default', ['js', 'css', 'dist']);
